3893e8a5c225242860553d1b166c978d6c73d635
amir <aa.midas.rus@gmail.com>
2024-11-26 14:17:16 +0600

    Added ChatGPT

---
+++ Notes/ChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatViewController.swift
+//  Notes
+//
+//  Created by iMac-10 on 26/11/24.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Notes/secrets.swift
@@ -0,0 +1,8 @@
+//
+//  secrets.swift
+//  Notes
+//
+//  Created by iMac-10 on 26/11/24.
+//
+
+import Foundation
